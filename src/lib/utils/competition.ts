d3a6c4b84ac4fd2efa88936e3df6faf49d7ae530
shawn <shawn@shawnui-MacBookPro.local>
2026-03-30 17:27:26 +0900

    Revert "feat: migrate mirip v2 mock frontend to sveltekit"
    This reverts commit ceec4b7cf3147fde3fb5ae76556fa878c2ae72b3.

--- src/lib/utils/competition.ts
+++
@@ -1,9 +0,0 @@
-import type { CompetitionFilter, CompetitionItem } from '$lib/types';
-
-export function filterCompetitions(items: CompetitionItem[], filter: CompetitionFilter) {
-	if (filter === '전체') {
-		return items;
-	}
-
-	return items.filter((item) => item.category === filter);
-}
